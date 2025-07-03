21187c05fda8e7d48a466a143108d76c8524beba
emotservices <nikesh@emotservices.com>
2025-07-03 15:19:13 +0530

    Update homepage Open Graph and meta description for EMaaS with new content and share image

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,24 @@
 import Home from "./(homes)/home-2/page";
 export const metadata = {
-  title: "EAMOT - Maintainance Tech with EMaaS and CMMS",
+  title:
+    "EAMOT - Maintainance Tech with EMaaS and CMMS - Innovating in Energy Management",
   description:
-    "With EAMOT, you can manage your assets, schedule maintenance, and track performance all in one place.",
+    "We leverage the latest technologies, including IoT and AI, to provide real-time data and predictive analytics that enable smarter energy management.",
+  openGraph: {
+    title:
+      "EAMOT - Maintainance Tech with EMaaS and CMMS - Innovating in Energy Management",
+    description:
+      "We leverage the latest technologies, including IoT and AI, to provide real-time data and predictive analytics that enable smarter energy management.",
+    images: [
+      {
+        url: "/assets/images/share_image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "EAMOT Share Image",
+      },
+    ],
+    type: "website",
+  },
 };
 export default function page() {
   return (
